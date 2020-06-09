5886cfbe74eafa7afc7d18ace9a755c323aa7607
Pooja Kankani <kankani2@illinois.edu>
2020-06-09 00:24:34 -0500

    Minor updates.

--- flutter/lib/pages/team_match.dart
+++ flutter/lib/pages/team_match.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:handcricket/constants.dart';
 
 class TeamMatchPage extends StatefulWidget {
   @override
@@ -8,7 +9,14 @@ class TeamMatchPage extends StatefulWidget {
 class _TeamMatchPageState extends State<TeamMatchPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Scaffold(
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          child: Text('Team match page'),
+        ),
+      ),
+    );
   }
 }

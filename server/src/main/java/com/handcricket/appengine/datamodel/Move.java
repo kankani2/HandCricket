43d86dcb689ebdc0b44a5d65901c1dcecbc2b0e3
Pooja Kankani <kankani2@illinois.edu>
2020-06-14 23:29:17 -0500

    Added move model for bat/bowl endpoints body. (#26)

---
+++ server/src/main/java/com/handcricket/appengine/datamodel/Move.java
@@ -0,0 +1,19 @@
+package com.handcricket.appengine.datamodel;
+
+public class Move {
+    private int num;
+
+    public Move(){}
+
+    public Move(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}

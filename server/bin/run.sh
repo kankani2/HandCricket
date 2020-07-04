dc8114449a2e84c62203c7d2ab7e0340ff28b841
Ayush Ranjan <ayushr2@illinois.edu>
2020-07-04 14:06:42 -0500

    update endpoints version

--- server/bin/run.sh
+++ server/bin/run.sh
@@ -2,7 +2,7 @@
 
 # Needed by Google Endpoints java Framework
 export ENDPOINTS_SERVICE_NAME=resounding-sled-278521.appspot.com
-export ENDPOINTS_SERVICE_VERSION=2020-06-09r0
+export ENDPOINTS_SERVICE_VERSION=2020-07-04r0
 
 # Regenerate target directory.
 mvn clean
